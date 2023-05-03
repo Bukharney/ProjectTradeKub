fd884b9ed1958fba929c6a92363821916397d091
Peerapat Padtawaro <peerapad.xiv@mail.kmutt.ac.th>
2023-05-03 13:51:52 +0700

    Update Log/in page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
   <React.StrictMode>
     <LoginPage/>
   </React.StrictMode>
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
