c9a7d3341280b6d0e8738e1bf82812192cc0cb59
Peerapat Padtawaro <peerapad.xiv@mail.kmutt.ac.th>
2023-05-03 18:56:50 +0700

    Log in

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import LoginPage from './component/LoginPage';
 import reportWebVitals from './reportWebVitals';
+import { useHistory } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
