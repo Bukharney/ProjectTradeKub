e8f2e847f44bd055ec7ecc04c192a260516c0e56
Peerapat Padtawaro <peerapad.xiv@mail.kmutt.ac.th>
2023-05-05 12:44:17 +0700

    Update Loge-in page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginPage from './component/Login';
+import Login from './component/Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginPage />
+    <Login />
   </React.StrictMode>
 );
 
