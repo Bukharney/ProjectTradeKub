f663aebc0a4745dd2887a669fdcd50c3524394bf
Peerapat Padtawaro <peerapad.xiv@mail.kmutt.ac.th>
2023-05-08 14:54:55 +0700

    Structure + Log-in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import Navbar from "./component/Navbar";
+import Login from "./component/Login/Login";
+//import Navbar from "./component/Navbar";
+//import Home from "./component/Home/Home";
+//import Dashboard from "./component/Dashboard/Dashboard";
+//import News from "./component/News/News";
+//import Wallet from "./component/Wallet/Wallet";
+//import Profle from "./component/Profile/Profile";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Login />
     </>
   );
 }
---
+++ frontend/src/component/Login/DB_Login.js
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    brokerId: "Broker1",
+    accountId: "CPE",
+    password: "241",
+  },
+  {
+    brokerId: "Broker2",
+    accountId: "KMUTT",
+    password: "231",
+  },
+];
