d0aca3050f90c5b67390bd52358684de5aecb108
Jirapat <bukharney@gmail.com>
2023-05-16 14:13:32 +0700

    mock

--- frontend/src/Constans/Mock.js
+++ frontend/src/Constans/Mock.js
@@ -20,21 +20,6 @@ export const mockSeachResult = {
   count: 4,
 };
 
-export const mockCompanyDetails = {
-  country: "US",
-  currency: "USD",
-  exchange: "NASDAQ/NMS (GLOBAL MARKET)",
-  ipo: "1980-12-12",
-  marketCapitalization: 1415993,
-  name: "Apple Inc",
-  phone: "14089961010",
-  shareOutstanding: 4375.47998046875,
-  ticker: "AAPL",
-  weburl: "https://www.apple.com/",
-  logo: "https://static.finnhub.io/logo/87cb30d8-80df-11ea-8951-00000000092a.png",
-  finnhubIndustry: "Technology",
-};
-
 export const mockStockQuote = {
   instrumentType: "STOCK",
   symbol: "AOT",
