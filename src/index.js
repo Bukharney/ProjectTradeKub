8844f3f45be9c8b59801b4cd367ff04681589c55
BigZ <pongsakornjansanit@hotmail.com>
2023-05-27 21:22:40 +0700

    SelectAccDB

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import SelectAcc from './component/Page/SelectAccount/SelectAcc';
+import Register from './component/Page/Register/Register';
 const root = createRoot(document.getElementById('root'));
 root.render(<SelectAcc />);
