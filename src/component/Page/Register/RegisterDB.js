0b0a68cb9b724cb7b608a28ea2038d09802946ac
BigZ <pongsakornjansanit@hotmail.com>
2023-05-27 22:13:08 +0700

    nidnoy

---
+++ src/component/Page/Register/RegisterDB.js
@@ -0,0 +1,15 @@
+const RegisterDataBase = [
+    {
+      email: "user1@example.com",
+      phoneNumber: "0625124581",
+      username: "user1",
+      password: "password1",
+    },
+    {
+      email: "user2@example.com",
+      phoneNumber: "0824569817",
+      username: "user2",
+      password: "password2",
+    }
+]
+  
--- src/component/Page/SelectAccount/AccountDB.js
+++ src/component/Page/SelectAccount/AccountDB.js
@@ -27,3 +27,7 @@ export const Account = {
       }
     ]
   };
+
+export const User = {
+
+};
