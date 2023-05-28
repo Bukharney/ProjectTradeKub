20a3de88ab5b138964d3265a424b6705b9e94908
BigZ <pongsakornjansanit@hotmail.com>
2023-05-28 17:30:48 +0700

    EditUserProfile

---
+++ src/component/Page/EditUserProfile/UserDB.js
@@ -0,0 +1,10 @@
+export const Username = {
+    username: [
+      {
+        email: "BigZ@example.com",
+        phonenumber: "0620546629",
+        username: "BigZPeter02",
+        password: "123456789BIGZ"
+      }
+    ]
+  };
